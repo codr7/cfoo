a517d081e0cdc0713b384d60780da77915540c2e
codr7 <andreas.gone.wild@gmail.com>
2019-12-25 22:04:08 +0100

    fixed parse hang

--- source/cfoo/parse.c
+++ source/cfoo/parse.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 #include "cfoo/id.h"
 #include "cfoo/parse.h"
 #include "cfoo/status.h"
@@ -7,9 +8,8 @@ struct cf_status *cf_parse(const char *in,
 			   struct cq_deque *out,
 			   const char **end) {
   struct cf_status *s = NULL;
-  const char *_end = in;
-  while (*_end && (s = cf_parse_token(in, out, &_end)) == cf_ok());
-  if (end) { *end = _end; }
+  while (*in && (s = cf_parse_token(in, out, &in)) == cf_ok());
+  if (end) { *end = in; }
   return s;
 }
 
@@ -17,7 +17,7 @@ struct cf_status *cf_parse_token(const char *in,
 				 struct cq_deque *out,
 				 const char **end) {
   char c = *in;
-
+  
   switch (c) {
   case 0:
     if (end) {
